2afb62f57e3336fa2a134ad61afb612ce7077707
MatteoArt <mattrim8@gmail.com>
2023-07-06 16:55:55 +0200

    completo jsnacks1

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -3,7 +3,23 @@ il Grande Gatsby ha una lista di nomi e una lista di cognomi, e
 da queste vuole generare una falsa lista di invitati con nome e cognome.
 */
 
-let nomi = ["Matteo","Michele","Giulia","Riccardo","Pietro","Pasquale","Salvatore",
+const listEl = document.querySelector(".lista");
+
+const nomi = ["Matteo","Michele","Giulia","Riccardo","Pietro","Pasquale","Salvatore",
 "Lorenzo"];
 
-let cognomi = ["Artizzu",]
+const cognomi = ["Artizzu","Piccinno","Corrado","Carluccio","Leone","Isabelli",
+"Bono","Porrega"];
+
+const lunghezza_arr = nomi.length;  //8
+
+for (let i = 0; i < lunghezza_arr; i++) {
+    //genero ad ogni giro due numeri randomici, uno per il nome e uno per il cognome
+    let rand1 = Math.floor(Math.random() * lunghezza_arr);
+    let rand2 = Math.floor(Math.random() * lunghezza_arr);
+    let nome_random = nomi[rand1]; //estraggo nome casuale
+    let cogn_random = cognomi[rand2]; //cognome casuale
+    console.log(nome_random + " " + cogn_random);
+
+    listEl.innerHTML += `<li>${nome_random} ${cogn_random}</li>`;
+}
