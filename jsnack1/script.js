3783a31297845b9e34318cf9327146bbf20e6923
MatteoArt <mattrim8@gmail.com>
2023-07-06 16:16:28 +0200

    inizio jssnack1

---
+++ jsnack1/script.js
@@ -0,0 +1,9 @@
+/* Generatore di “nomi cognomi” casuali:
+il Grande Gatsby ha una lista di nomi e una lista di cognomi, e 
+da queste vuole generare una falsa lista di invitati con nome e cognome.
+*/
+
+let nomi = ["Matteo","Michele","Giulia","Riccardo","Pietro","Pasquale","Salvatore",
+"Lorenzo"];
+
+let cognomi = ["Artizzu",]
